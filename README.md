f3d664ccc2d9a879340e00bf670ab6787bd24a95
MuradBozik <murad.bozik@gmail.com>
2021-11-17 01:03:55 +0100

    all tasks are implemented

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # AiDM-A2
+
+Basic steps for each measure are;
+1. Create the sparse input matrix
+2. Check the measure
+   - Jaccard Distance
+     * Create random permutations of the rows
+     * For each column (user) find the index of first non-zero rating 
+     * Create the minhash signature matrix M 
+   - Cosine Distance
+     * Create random vectors with uniform distribution between ```[-1,+1]```, ```shape=(h, movie_number)```
+     * Using dot product with input matrix, create sketch matrix M
+   - Discrete Cosine Distance 
+     * Create random vectors with uniform distribution between ```[-1,+1]```, ```shape=(h, movie_number)```
+     * Modify input matrix as non-zero elements = 1
+     * Using dot product with modified input matrix, create sketch matrix M
+3. Calculate total number of bands could be generated using given row values
+4. Select randomly b bands from them
+5. For each band of matrix M use a hash function to divide each User into one bucket
+6. Get candidates by filtering bucket elements ``` len(bucket[key]) >= 2 ```
+7. Check candidate pairs by calculating distance between them and filter by threshold
+8. Write similar pairs into txt file
+
+
+Implementation notes
+* Creating sparse input matrix takes too long (7 mins). Not to repeat this process each run, 
+after creating the sparse input matrix, We saved it as **sparse_input_matrix.npz** in data folder. 
+The process of creating this matrix can also be seen in our code as commented out way.
+* For banding strategy we decided not to use overlapping bands. So ```b*r <= M.shape[0]```. 
+* Buckets is a dictionary object, key is bucket index created by hash function and value is set of user ids which is actually column index of input matrix
+* We are using huge number of buckets, but dictionary object doesn’t store any empty bucket
+* We didn’t store the row permutations, we permuted and calculated using one pass on all rows
+* We created one hash function to divide users into buckets, the parameters are randomly selected. Since we used numpy seed value it would create same hash function in every run.
+
+Experimentation notes
+- [ ] Use different number of parameters (h, b, r)
+- [ ] Monitor the time of calculation for each method. If it is more than 30 mins per measure tune parameters or change the strategy of checking candidate pairs.
+- [ ] Repeat experiments with different seed values to see consistency of algorithm.
+- [ ] Make analysis of results of different experiments for each measure
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import argparse
 import warnings
 import numpy as np
 import pandas as pd
-from scipy.sparse import csr_matrix
+from itertools import combinations
+from scipy.sparse import csr_matrix, save_npz, load_npz
+from scipy.spatial.distance import jaccard, cosine
+
 
 def parse_args():
     # Parse command line arguments
@@ -17,69 +20,110 @@ def parse_args():
     return ap.parse_args()
 
 
-class LSH:
-    def __init__(self, args, S, h, r, b):
-        self.args = args
-        self.h = h # signature length
-        self.r = r # number of rows in one band
-        self.b = b # number of bands
-        self.S = S # input matrix
-        self.M = self.create_signature_matrix(h,S)
+def minhash_signatures(k , S):
+    M = np.zeros((k, S.shape[1]))
+    for i in range(k):
+        rxs = np.random.permutation(S.shape[0])
+        M[i,:] = (S[rxs,:] != 0).argmax(axis=0)
+    return M
 
-    def create_signature_matrix(self, k , S):
-        M = np.zeros((k, S.shape[1]))
-        for i in range(k):
-            rxs = np.random.permutation(S.shape[0])
-            M[i,:] = (S[rxs,:] != 0).argmax(axis=0)
-        return M
+def random_projection_signatures(h, S, measure='cs'):
+    if measure == "dcs":
+        S[S.nonzero()] = 1
+    V = np.random.uniform(-1, 1, size=(h, S.shape[0]))
+    sketch_matrix = np.dot(V, S)
+    return sketch_matrix
 
-    def select_bands(self):
-        total_bands = self.M.shape[0]//self.r
-        selected_bands = np.random.choice(total_bands, self.b, replace=False)
-        selected_bands.sort()
-        return selected_bands
+def select_bands(M, r, b):
+    total_bands = M.shape[0]//r
+    selected_bands = np.random.choice(total_bands, b, replace=False)
+    selected_bands.sort()
+    return selected_bands
 
-    def get_hash_func(self, bucket_num):
-        a = np.random.randint(1, 10**6, size=self.r)
-        b = np.random.randint(1, 10**6)
-        return lambda x: (np.dot(a, x) + b) % bucket_num
+def get_hash_func(r, bucket_num):
+    a = np.random.randint(1, 10**6, size=r)
+    b = np.random.randint(1, 10**6)
+    return lambda x: (np.dot(a, x) + b) % bucket_num
 
-    
+def add_to_buckets(buckets, hashed):
+    for i, val in enumerate(hashed):
+        try:
+            buckets[val].add(i)
+        except KeyError:
+            buckets[val] = {i}
 
+def filterBuckets(buckets, minVal=2):
+    for (key, value) in buckets.items():
+        if len(value) < minVal:
+            del buckets[key]
 
+def j_sim(u1, u2):
+    return 1 - jaccard(u1, u2)
 
+def c_sim(u1, u2):
+    return 1 - cosine(u1, u2)
 
+def check_candidates(buckets, M, callback, threshold):
+    ps = set()
+    for key, val in buckets.items():
+        c_pairs = combinations(val)
+        for p in c_pairs:
+            if callback(M[:, p[0]], M[:, p[1]]) > threshold:
+                ps.add(p)
+    return ps
+
+def write_down(pairs_set, measure="js"):
+    pairs_list = list(pairs_set)
+    pairs_list.sort()
+    pairs = [f"{p[0]}, {p[1]}\n" for p in pairs_list]
+    outfile = f"{measure}.txt"
+    with open(outfile, "w") as f:
+        f.writelines(pairs)
 
 
 def main(args):
     np.random.seed(args.s)
+    
+    # data = np.load("data/user_movie_rating.npy")
+    # data = data[:, [1, 0, 2]]
+    # data = data[np.argsort(data[:, 0])]
+    #
+    # cols = ["movie_id", "user_id", "rating"]
+    # df = pd.DataFrame(data, columns=cols)
+    # df = df.pivot(index="movie_id", columns="user_id", values="rating")
+    # df = df.fillna(0)
+    # S = csr_matrix(df.to_numpy())
+    # save_npz("data/sparse_input_matrix.npz", S)
 
-    # We are creating a movies-users matrix
-    data = np.load("data/user_movie_rating.npy")
-    data = data[:, [1, 0, 2]]
-    data = data[np.argsort(data[:, 0])]
+    S = load_npz("data/sparse_input_matrix.npz")
+    # Parameters
+    h = 100 # signature length
+    r = 15  # number of rows in one band
+    b = 6  # number of bands
+    N = 10**9 # number of buckets
 
-    cols = ["movie_id", "user_id", "rating"]
-    df = pd.DataFrame(data, columns=cols)
-    df = df.pivot(index="movie_id", columns="user_id", values="rating")
-    df = df.fillna(0)
-    S = csr_matrix(df.to_numpy())
+    buckets = dict()
 
+    if args.m.lower() == "js":
+        M = minhash_signatures(h, S)
+        threshold = 0.5
+        sim_func = j_sim
+    else:
+        M = random_projection_signatures(h, S, measure= args.m.lower())
+        threshold = 0.73
+        sim_func = c_sim
 
+    hs = get_hash_func(r, N)
+    bands = select_bands(M, r, b)
 
+    for band in bands:
+        M_slice = M[band*r:band*r + r, :]
+        hashed = np.apply_along_axis(func1d=hs, axis=0, arr=M_slice)
+        add_to_buckets(buckets, hashed)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    filterBuckets(buckets, minVal=2)
+    similar_pairs = check_candidates(buckets, M, sim_func, threshold)
+    write_down(similar_pairs, args.m.lower())
 
 
 if __name__ == "__main__":
