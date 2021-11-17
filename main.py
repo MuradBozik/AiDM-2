53c0f22066e9ddb0fc87b0f138f7546cae18ccc1
MuradBozik <murad.bozik@gmail.com>
2021-11-17 17:06:52 +0100

    Errors are solved

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import warnings
 import numpy as np
 import pandas as pd
+from datetime import datetime
 from itertools import combinations
 from scipy.sparse import csr_matrix, save_npz, load_npz
 from scipy.spatial.distance import jaccard, cosine
@@ -10,11 +11,11 @@ from scipy.spatial.distance import jaccard, cosine
 def parse_args():
     # Parse command line arguments
     ap = argparse.ArgumentParser(description="Assignment 2: Implementing Locality Sensitive Hashing")
-    ap.add_argument("-d", type=str, default="./data/user_movie_rating.npy", required=True,
+    ap.add_argument("-d", "--data", type=str, default="./data/user_movie_rating.npy", required=True,
                     help="Path to data file")
-    ap.add_argument("-s", "--seed", type=int, default=25, required=True,
+    ap.add_argument("-s", "--seed", type=int, default=25,
                     help="Random seed (by using np.random.seed(int))")
-    ap.add_argument("-m", "--measure", type=str, default="js",
+    ap.add_argument("-m", "--measure", type=str, default="js", choices=["js", "cs", "dcs"],
                     help="Similarity measure (js / cs / dcs) – "
                          "selects which similarity measure to use (either Jaccard, cosine or discrete cosine)")
     return ap.parse_args()
@@ -31,8 +32,10 @@ def random_projection_signatures(h, S, measure='cs'):
     if measure == "dcs":
         S[S.nonzero()] = 1
     V = np.random.uniform(-1, 1, size=(h, S.shape[0]))
-    sketch_matrix = np.dot(V, S)
-    return sketch_matrix
+    V = csr_matrix(V) # if we don't use this line following dot product takes up to 120GB in memory
+    # and automatically kills the process.
+    sketch_matrix = V.dot(S)
+    return sketch_matrix.toarray()
 
 def select_bands(M, r, b):
     total_bands = M.shape[0]//r
@@ -41,9 +44,9 @@ def select_bands(M, r, b):
     return selected_bands
 
 def get_hash_func(r, bucket_num):
-    a = np.random.randint(1, 10**6, size=r)
-    b = np.random.randint(1, 10**6)
-    return lambda x: (np.dot(a, x) + b) % bucket_num
+    a = np.random.randint(bucket_num, bucket_num*100, size=r)
+    b = np.random.randint(bucket_num, bucket_num*100)
+    return lambda x: int((np.dot(a, x) + b) % bucket_num)
 
 def add_to_buckets(buckets, hashed):
     for i, val in enumerate(hashed):
@@ -53,9 +56,12 @@ def add_to_buckets(buckets, hashed):
             buckets[val] = {i}
 
 def filterBuckets(buckets, minVal=2):
+    keys = list()
     for (key, value) in buckets.items():
         if len(value) < minVal:
-            del buckets[key]
+            keys.append(key)
+    for key in keys:
+        del buckets[key]
 
 def j_sim(u1, u2):
     return 1 - jaccard(u1, u2)
@@ -66,7 +72,7 @@ def c_sim(u1, u2):
 def check_candidates(buckets, M, callback, threshold):
     ps = set()
     for key, val in buckets.items():
-        c_pairs = combinations(val)
+        c_pairs = combinations(val, 2)
         for p in c_pairs:
             if callback(M[:, p[0]], M[:, p[1]]) > threshold:
                 ps.add(p)
@@ -82,7 +88,7 @@ def write_down(pairs_set, measure="js"):
 
 
 def main(args):
-    np.random.seed(args.s)
+    np.random.seed(args.seed)
     
     # data = np.load("data/user_movie_rating.npy")
     # data = data[:, [1, 0, 2]]
@@ -95,21 +101,21 @@ def main(args):
     # S = csr_matrix(df.to_numpy())
     # save_npz("data/sparse_input_matrix.npz", S)
 
-    S = load_npz("data/sparse_input_matrix.npz")
+    S = load_npz(args.data)
     # Parameters
     h = 100 # signature length
     r = 15  # number of rows in one band
     b = 6  # number of bands
-    N = 10**9 # number of buckets
+    N = 10**5# number of buckets
 
     buckets = dict()
 
-    if args.m.lower() == "js":
+    if args.measure == "js":
         M = minhash_signatures(h, S)
         threshold = 0.5
         sim_func = j_sim
     else:
-        M = random_projection_signatures(h, S, measure= args.m.lower())
+        M = random_projection_signatures(h, S, measure= args.measure)
         threshold = 0.73
         sim_func = c_sim
 
@@ -122,8 +128,9 @@ def main(args):
         add_to_buckets(buckets, hashed)
 
     filterBuckets(buckets, minVal=2)
+
     similar_pairs = check_candidates(buckets, M, sim_func, threshold)
-    write_down(similar_pairs, args.m.lower())
+    write_down(similar_pairs, args.measure)
 
 
 if __name__ == "__main__":
@@ -131,4 +138,7 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
 
     args = parse_args()
+    t0 = datetime.now()
     main(args)
+    t1 = datetime.now()
+    print(f"Total Time: {str(t1 - t0)}")
